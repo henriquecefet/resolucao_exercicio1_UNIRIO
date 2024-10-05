5b8598cc28a72d3eb2459c407ae280d9709a967b
Henrique Soares Rodrigues <rodriguesoareshenrique@gmail.com>
2024-10-05 15:41:13 -0300

    Atualizando a classe ContaBancaria

--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -2,29 +2,28 @@
 public class ContaBancaria {
     private double saldo;
     private static double taxa = 1.50;
-
     public ContaBancaria(double saldoInicial) {
         this.saldo = saldoInicial;
     }
-
     public void depositar(double valor) {
         if (valor > 0) {
             saldo += valor;
         }
     }
-
     public boolean sacar(double valor) {
-        if (valor + taxa > 0 && valor <= saldo) {
-            saldo -= taxa;
-            saldo -= valor;
+        double novoSaldo = saldo - valor - taxa;
+        if (novoSaldo > 0) {
+            this.setSaldo(novoSaldo);
             return true;
         } else {
             return false;
         }
     }
-
     public double getSaldo() {
         return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
     }
 
 }
